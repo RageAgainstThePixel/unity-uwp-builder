4a139431e129dab42a4c048d7b2f3be1b480801e
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2024-09-07 15:54:10 -0400

    .

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import core = require('@actions/core');
+import exec = require('@actions/exec');
+import glob = require('@actions/glob');
+import path = require('path');
 
 const main = async () => {
     try {
-        core.info('Hello World!');
+        if (process.platform !== `win32`) { throw new Error(`This action can only run on Windows runner.`); }
+        const projectPath = core.getInput(`project-path`, { required: true });
+        const globber = await glob.create(path.join(projectPath, `**/*.sln`));
+        const files = await globber.glob();
+        if (files.length === 0) { throw new Error(`No solution or project file found.`); }
+        const buildPath = files[0];
+        core.info(`Building ${buildPath}`);
+        const configuration = core.getInput(`configuration`, { required: true });
+        const additionalArgs = core.getInput(`additional-args`);
+        const buildArgs = [`/t:Build`, `/p:Configuration=${configuration}`, ...additionalArgs.split(` `)];
+        await exec.exec(`msbuild`, [buildPath, ...buildArgs]);
     } catch (error) {
         core.setFailed(error);
     }
