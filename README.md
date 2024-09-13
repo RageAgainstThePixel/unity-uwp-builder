74fb30707b18d238110fdba8df30999e47ee5a3a
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2024-09-13 08:43:24 -0400

    unity-uwp-builder@v1.0.2 (#5)
    - added `certificate-path` input parameter
    - updated docs and validation tests

--- README.md
+++ README.md
@@ -11,10 +11,15 @@ A GitHub Action to build Unity exported UWP projects.
 
 ```yaml
 steps:
+  # required for unity-uwp-builder action
+  - uses: microsoft/setup-msbuild@v2
+
+  # builds visual studio project for UWP and packages it for store upload
   - uses: RageAgainstThePixel/unity-uwp-builder@v1
     id: uwp-build
     with:
       project-path: '/path/to/your/build/output/directory'
+      architecture: 'ARM64'
       package-type: 'upload'
 
   - name: print outputs
@@ -33,6 +38,7 @@ steps:
 | `configuration` | The configuration to use when building the visual studio project. | Defaults to `Master`. |
 | `architecture` | The architecture to use when building the visual studio project. Can be: `x86`, `x64`, `ARM`, or `ARM64`. | Defaults to `ARM64`. |
 | `package-type` | The type of package to generate. Can be: `sideload` or `upload`. | Defaults to `sideload`. |
+| `certificate-path` | The path to the certificate to use when packaging the UWP project. | Required when `package-type` is `sideload`. Defaults to the Unity generated test certificate. |
 
 ### outputs
 
--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,11 @@ const main = async () => {
         if (files.length === 0) { throw new Error(`No solution file found.`); }
         const buildPath = files[0];
         core.info(`Building ${buildPath}`);
+        const appPackagesPath = path.join(projectPath, `AppPackages`);
+        if (fs.existsSync(appPackagesPath)) {
+            core.info(`Cleaning AppPackages directory: ${appPackagesPath}`);
+            await fs.promises.rm(appPackagesPath, { recursive: true, force: true });
+        }
         let projectName = path.basename(buildPath, `.sln`);
         core.info(`projectName: "${projectName}"`);
         const configuration = core.getInput(`configuration`, { required: true });
@@ -37,8 +42,13 @@ const main = async () => {
                 );
                 break;
             case `sideload`:
+                const certificatePath = await getCertificatePath(projectPath);
+                const thumbprint = await getCertificateThumbprint(certificatePath);
                 buildArgs.push(
-                    `/p:UapAppxPackageBuildMode=SideloadOnly`
+                    `/p:UapAppxPackageBuildMode=SideloadOnly`,
+                    `/p:AppxPackageSigningEnabled=true`,
+                    `/p:PackageCertificateThumbprint=${thumbprint}`,
+                    `/p:PackageCertificateKeyFile="${certificatePath}"`
                 );
                 break;
             default:
@@ -92,3 +102,33 @@ const main = async () => {
 }
 
 main();
+
+async function getCertificatePath(projectPath: string): Promise<string> {
+    let certificatePath = core.getInput(`certificate-path`) || `${projectPath}/**/*.pfx`;
+    const certificateGlobber = await glob.create(certificatePath);
+    const certificateFiles = await certificateGlobber.glob();
+    switch (certificateFiles.length) {
+        case 0:
+            throw new Error(`No certificate file found. Please set the 'certificate-path' input.`);
+        default:
+            if (certificateFiles.length > 1) {
+                core.warning(`More than one certificate file found, using the first one found:\n${certificateFiles.join(`\n`)}`);
+            }
+            certificatePath = certificateFiles[0];
+    }
+    await fs.promises.access(certificatePath, fs.constants.R_OK);
+    return certificatePath;
+}
+
+async function getCertificateThumbprint(certificatePath: string): Promise<string> {
+    const thumbprintCmd = `powershell -command "(Get-PfxCertificate -FilePath '${certificatePath}').Thumbprint"`;
+    let thumbprint = ``;
+    await exec.exec(thumbprintCmd, [], {
+        listeners: {
+            stdout: (data: Buffer) => {
+                thumbprint += data.toString();
+            }
+        }
+    });
+    return thumbprint.trim();
+}
